ff6745350b4ab8521c4c37c6ab2b2828eb280599
kangseokjooo <zpckq69@naver.com>
2024-05-13 11:41:02 +0900

    Java:Algorithm

--- Java_Algorithm/src/java_algorithm_study/Day51_1.java
+++ Java_Algorithm/src/java_algorithm_study/Day51_1.java
@@ -6,28 +6,28 @@ import java.util.Scanner;
 
 public class Day51_1 {
     static char[][] map;
-    static boolean visited[][];
-    static int N, M;
+    static boolean[][] visited;
+    static int M, N;
     static int[] dx = { -1, 1, 0, 0 };
-    static int dy[] = { 0, 0, -1, 1 };
+    static int[] dy = { 0, 0, -1, 1 };
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         N = s.nextInt(); 
         M = s.nextInt(); 
         s.nextLine();
-        map = new char[N][M]; 
-        visited = new boolean[N][M]; 
-        for (int i = 0; i < N; i++) { 
+        map = new char[M][N];
+        visited = new boolean[M][N];
+        for (int i = 0; i < M; i++) {
             String row = s.nextLine();
-            for (int j = 0; j < M; j++) { 
+            for (int j = 0; j < N; j++) {
                 map[i][j] = row.charAt(j);
             }
         }
         int mp = 0;
         int ep = 0;
-        for (int i = 0; i < N; i++) { 
-            for (int j = 0; j < M; j++) { 
+        for (int i = 0; i < M; i++) { 
+            for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
                     int p = bfs(i, j);
                     if (map[i][j] == 'W') {
@@ -47,13 +47,13 @@ public class Day51_1 {
         visited[x][y] = true;
         int cnt = 1;
         while (!q.isEmpty()) {
-            int cur[] = q.poll();
+            int[] cur = q.poll();
             int curX = cur[0];
             int curY = cur[1];
             for (int i = 0; i < 4; i++) {
                 int nx = curX + dx[i];
                 int ny = curY + dy[i];
-                if (nx >= 0 && nx < M && ny < N && ny >= 0 && !visited[nx][ny] && map[nx][ny] == map[x][y]) {
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && map[nx][ny] == map[x][y]) {
                     q.offer(new int[] { nx, ny });
                     visited[nx][ny] = true;
                     cnt++;
