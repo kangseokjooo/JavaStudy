bbea90fb7cff8f1079369262c7b8c7cf2eb002f0
kangseokjooo <zpckq69@naver.com>
2024-06-27 12:07:41 +0900

    Java:Algorithm

---
+++ Java_Algorithm/src/java_algorithm_study/Day72_1.java
@@ -0,0 +1,49 @@
+package java_algorithm_study;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Day72_1 {
+	static class Pos{
+		int idx,jumps;
+		Pos(int idx,int jumps){
+			this.idx=idx;
+			this.jumps=jumps;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int A[]=new int[N];
+		
+		for(int i=0;i<N;i++) {
+			A[i]=s.nextInt();
+		}
+		int result=bfs(A,N);
+		System.out.println(result);
+		
+	}
+	public static int bfs(int []A,int N) {
+		Queue<Pos> q=new LinkedList<>();
+		boolean visited[]=new boolean[N];
+		q.offer(new Pos(0,0));
+		visited[0]=true;
+		
+		while(!q.isEmpty()) {
+			Pos cur=q.poll();
+			
+			if(cur.idx==N-1) {
+				return cur.jumps;
+			}
+			for(int i=1;i<=A[cur.idx];i++) {
+				int nIdx=cur.idx+i;
+				if(nIdx<N && !visited[nIdx]) {
+					q.offer(new Pos(nIdx,cur.jumps+1));
+					visited[nIdx]=true;
+				}
+			}
+		}
+		return -1;
+	}
+}
